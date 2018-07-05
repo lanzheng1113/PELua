33b86a0140101f43fb208693a8bc53425e989f6d
duocore_c <lanzheng_1123@163.com>
2018-07-05 18:14:50 +0800

    增加PEExt.EnableAppleTrackPadRightClick用于启用MACBOOK右键点击。

--- PELuaTool/PELuaTool/luaextPE.cpp
+++ PELuaTool/PELuaTool/luaextPE.cpp
@@ -123,6 +123,57 @@ int LFInitEnvironmentVariable(lua_State* l)
 	return 0;
 }
 
+
+static int EnableAppleTrackRightClick(BOOL bEnable)
+{
+	HANDLE hAppleTrackPad = CreateFile(_T("\\\\.\\AppleTrackpad"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+	if (INVALID_HANDLE_VALUE == hAppleTrackPad)
+	{
+		LOG_ERROR("打开设备\\\\.\\AppleTrackpad失败了，请确认是否没有安装。错误代码=%d", GetLastError());
+		return 1;
+	}
+	if (!bEnable)
+	{
+		DWORD disableValue = 0x28;
+		DWORD byteReturned = 0;
+		BOOL bDisable = DeviceIoControl(hAppleTrackPad, 0xF2004, &disableValue, sizeof(disableValue), NULL, NULL, &byteReturned, NULL);
+		if (bDisable)
+		{
+			LOG_INFO("成功发送禁用AppleTrackPad右键的请求");
+			return 0;
+		}
+		else
+		{
+			LOG_ERROR("发送禁用AppleTrackPad右键的请求失败了(%d)", GetLastError());
+		}
+	}
+	else
+	{
+		DWORD enableValue = 0x68;
+		DWORD byteReturned = 0;
+		if (DeviceIoControl(hAppleTrackPad, 0xF2004, &enableValue, sizeof(enableValue), NULL, NULL, &byteReturned, NULL))
+		{
+			LOG_INFO("成功发送启用AppleTrackPad右键的请求");
+			return 0;
+		}
+		else
+		{
+			LOG_ERROR("发送启用AppleTrackPad右键的请求失败了(%d)", GetLastError());
+		}
+	}
+	return 1;
+}
+
+
+int LFEnableAppleTrackPadRightClick(lua_State* l)
+{
+	BOOL bEnable = lua_toboolean(l, 1);
+	int iRet = EnableAppleTrackRightClick(bEnable);
+	lua_pushinteger(l, iRet);
+	return 1;
+}
+
+
 static const struct luaL_Reg PEExtLib[] = {
 	{ "INIT", LFINIT },
 	{ "SHELL", LFSHELL },
@@ -133,6 +184,7 @@ static const struct luaL_Reg PEExtLib[] = {
 	{ "SetDesktopText", LFSetDesktopText },
 	{ "ShowUSBDisk", LFShowUSBDisk },
 	{ "InitEnvironmentVariable", LFInitEnvironmentVariable },
+	{ "EnableAppleTrackPadRightClick", LFEnableAppleTrackPadRightClick },
 	{ NULL, NULL },
 };
 
